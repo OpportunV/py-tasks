04d08a9d009f328245df43581b2b43a61f98632e
OpportunV <Vadya.95@mail.ru>
2020-01-06 05:36:26 +0300

    tasks app with some views and styles

--- pythontasks/tasks/views.py
+++ pythontasks/tasks/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+tmp_tasks = [
+    {
+        'author': 'first',
+        'title': 'title1',
+        'content': 'content1',
+        'date': 'Jan 2, 2020'
+    },
+    {
+        'author': 'first',
+        'title': 'title2',
+        'content': 'content2',
+        'date': 'Jan 3, 2020'
+    },
+]
+
+
+def index(request):
+    content = {
+        'tasks': tmp_tasks
+    }
+    return render(request, 'tasks/index.html', content)
+
+
+def about(request):
+    content = {
+        'title': 'About'
+    }
+    return render(request, 'tasks/about.html', content)
